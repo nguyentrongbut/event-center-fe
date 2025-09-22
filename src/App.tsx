6632ef1c49de0bcec20d6895640b137816755f74
nguyentrongbut <cloly031128@gmail.com>
2025-09-22 22:58:37 +0700

    setup: init prj with vite + taiwindcss + shadcnui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className='text-blue-300'>React Vite + TaiwindCss + Shadcn ui</div>
+    </>
+  )
+}
+
+export default App
